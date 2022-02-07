998989f934d99634c8ac03c345043d9a0171ba9d
DanielDarkmun <dape_18@alu.uabcs.mx>
2022-02-06 17:39:17 -0700

    Ejercicio 1 finalizado

---
+++ exercise4.py
@@ -0,0 +1,6 @@
+# Añadir el código necesario para crear una variable que guarde la velocidad del asteroide.
+asteroid = 45
+# Escribe una expresión de prueba para calcular si necesita una advertencia.
+if asteroid > 25:
+    # Agregue las instrucciones que se ejecutarán si la expresión de prueba es true o false.
+    print('Warning. The asteroid is approaching at high speed');
