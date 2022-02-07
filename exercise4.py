aaaf5baffd831b39e1db9aba366ba4d89851693e
DanielDarkmun <dape_18@alu.uabcs.mx>
2022-02-06 17:46:46 -0700

    Ejercicio 2 finalizado

--- exercise4.py
+++ exercise4.py
@@ -1,6 +1,9 @@
-# Añadir el código necesario para crear una variable que guarde la velocidad del asteroide.
-asteroid = 45
-# Escribe una expresión de prueba para calcular si necesita una advertencia.
-if asteroid > 25:
-    # Agregue las instrucciones que se ejecutarán si la expresión de prueba es true o false.
-    print('Warning. The asteroid is approaching at high speed');
+# Agrega el código para crear una variable para un asteroide que viaja a 19 km/s
+asteroid = 21
+# Escribe varias expresiones de prueba para determinar si puedes ver el rayo de luz desde la tierra
+if asteroid == 20:
+    print('Look up! see the asteroid ') 
+elif asteroid > 20:
+    print('Look up! see the asteroid ') 
+else:
+    print('Look up! see my finger') 
